23e86a6be1d8c4a48a26061ad940668e078f7285
muhammad-010 <malbae01@gmail.com>
2025-11-21 11:34:25 +0700

    Refactor frontend build to standard Vite SPA

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/main.js'],
-            refresh: true,
-        }),
-        vue(),
-    ],
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
+    build: {
+        outDir: 'dist',
+    }
 });
